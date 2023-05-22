3cd97619dd4163a998a7018352bc06bb61af84af
Eshwar Muneshar <Emuneshar@gmail.com>
2023-05-22 19:33:46 -0400

    Assigned HW for Iliyan

--- ExamReview.java
+++ ExamReview.java
@@ -1,8 +1,8 @@
 public class ExamReview {
     public static void main(String [] args){
-        String holiday = "New Years";
+        String holiday = "Easter";
 
-        switch(holiday){
+        switch(holiday.toLowerCase()){
             case "Easter":
                 System.out.println("Happy Easter!");
                 break;
@@ -22,6 +22,14 @@ public class ExamReview {
                 System.out.println("Have a great holiday!");
                 break;
         }
+
+
+        // for( declare our variable and assign its value; how far do you want the loop to go; how much you want to go up or down by)
+
+       int [] list = {1,2,3,4,5,6,7,7,6545,456,45,43,5,6,5,34,665,445,67,5445,645,6,54776,5,657,7};
+
+       // problem
+       // add one to every even index in this list
 
     }
 }
