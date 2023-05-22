8ca058ad22c8e84a65f848306c326f3f289585cc
Eshwar Muneshar <Emuneshar@gmail.com>
2023-05-22 19:04:57 -0400

    Practicing with switch case

--- ExamReview.java
+++ ExamReview.java
@@ -1,6 +1,27 @@
-public class Main {
-    public static void Main(String [] args){
-        // Question 1
-        // Create an array of 10 numbers 
+public class ExamReview {
+    public static void main(String [] args){
+        String holiday = "Thanksgiving";
+
+        switch(holiday){
+            case "Easter":
+                System.out.println("Happy Easter!");
+                break;
+            case "Fourth of July":
+                System.out.println("Happy 4th of July!");
+                break;
+            case "Memorial Day":
+                System.out.println("Happy Memorial Day!");
+                break;
+            case "Christmas":
+                System.out.println("Merry Christmas!");
+                break;
+            case "Thanksgiving":
+                System.out.println("Happy Thanksgiving!");
+                break;
+            default:
+                System.out.println("Have a great holiday!");
+                break;
+        }
+
     }
 }
